9908ae8233d5f832faa1512d44e361a072a00bc8
Gabriel Fernandes <hu3master.zord@hotmail.com>
2022-10-20 14:28:50 -0300

    exercicio if

---
+++ JavaScript/if-else.js
@@ -0,0 +1,8 @@
+const notas = 80;
+if (notas >= 80) {
+    console.log('Parabéns, você foi aprovada(o)!');
+} else if (notas < 80 && notas >= 60) {
+    console.log('Você está na nossa lista de espera');
+} else {
+    console.log('Você foi reprovada(o)');
+}
