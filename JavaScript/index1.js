0f14b56855d048bcb7c39c9ba97dc9e0f9eccf30
Gabriel Fernandes <hu3master.zord@hotmail.com>
2022-10-20 13:25:21 -0300

    primeira atividade

---
+++ JavaScript/index1.js
@@ -0,0 +1,11 @@
+const myName = 'Gabriel Fernandes';
+
+const birthCity = 'Natal/RN';
+
+let birthYear = 1999;
+birthYear = 2030;
+birthCity = 'Não pode-se modificar const';
+
+console.log(birthCity);
+console.log(birthYear);
+console.log(myName);
