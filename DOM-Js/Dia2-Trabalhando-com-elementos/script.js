4c7743ea1f52c64f503cd4505fab6b5bc32f6a8f
Gabriel Fernandes <hu3master.zord@hotmail.com>
2022-10-31 14:52:15 -0300

    atividade 2

--- DOM-Js/Dia2-Trabalhando-com-elementos/script.js
+++ DOM-Js/Dia2-Trabalhando-com-elementos/script.js
@@ -4,4 +4,23 @@ document.getElementById('primeiroFilhoDoFilho').innerText = 'alo mainha to aaaaa
 console.log(document.getElementById('pai').firstElementChild);
 console.log(document.getElementById('elementoOndeVoceEsta').nextElementSibling);
 console.log(document.getElementById('pai').lastElementChild.previousElementSibling);
-console.log(document.getElementById('elementoOndeVoceEsta').nextSibling)
+console.log(document.getElementById('elementoOndeVoceEsta').nextSibling);
+let astolfo = document.createElement('div');
+astolfo.innerText = 'mãe de deus';
+let paiDeTodos = document.getElementById('pai');
+paiDeTodos.appendChild(astolfo);
+
+let elementoEsta = document.getElementById('elementoOndeVoceEsta');
+let semiramis = document.createElement('section');
+semiramis.innerText = 'ronaldo'
+elementoEsta.appendChild(semiramis);
+
+let karna = document.createElement('section');
+let primeiroDoFi = document.getElementById('primeiroFilhoDoFilho');
+karna.innerText = 'karna o filho do deus sol';
+primeiroDoFi.appendChild(karna);
+
+console.log(document.getElementById(karna));
+
+const filhoDo = karna;
+console.log(filhoDo.parentElement.parentElement.nextElementSibling);
